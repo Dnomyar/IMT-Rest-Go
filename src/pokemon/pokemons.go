9cc40f737c0e4a938c32f59d9a8098a397a00403
Etienne <agahnim13@gmail.com>
2017-04-25 10:21:36 +0200

    Création de la liste, squelette du GET liste

--- src/pokemon/pokemons.go
+++ src/pokemon/pokemons.go
@@ -6,17 +6,35 @@ import (
 	"util"
 )
 
-var pokemons []Pokemon
+var pokemons [150]Pokemon
 
 func AddPokemon(w http.ResponseWriter, r *http.Request) {
-	pokemons.Append(&Pokemon{
-		Id: 0,
-		Name: r.URL.Path[1:],
-		Type: "truc",
-		Description: "awesome desc",
-	})
+	pokemons[24] = Pokemon{
+		Id: 25,
+		Name: "Pikachu",
+		Type: "Foudre",
+		Description: "La souris électrique",
+	}
+	pokemons[3] = Pokemon{
+		Id: 4,
+		Name: "Salamèche",
+		Type: "Feu",
+		Description: "La salamandre enflammée",
+	}
+	pokemons[6] = Pokemon{
+		Id: 7,
+		Name: "Bulbizarre",
+		Type: "Plante",
+		Description: "Le machin avec un bulbe",
+	}
+	pokemons[3] = Pokemon{
+		Id: 3,
+		Name: "Carapuce",
+		Type: "Eau",
+		Description: "La tortue qui te crache dessus",
+	}
 
 	res, err := json.Marshal(pokemons)
 
-	util.RespCreated(w, res, err)
+	util.RespJson(w, 200, res, err)
 }
